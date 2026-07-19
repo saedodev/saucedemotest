2f4c62654332bc1b6a68b0b9b6d270c8048382a8
Sayid Ridho <sayidridho65@gmail.com>
2026-07-19 11:03:05 +0800

    [Update] Change on DriverFactory for github actions

--- src/main/java/com/ridho/driver/DriverFactory.java
+++ src/main/java/com/ridho/driver/DriverFactory.java
@@ -21,22 +21,40 @@ public class DriverFactory {
 
             ChromeOptions options = new ChromeOptions();
 
+            // Incognito Mode
             options.addArguments("--incognito");
 
-            options.addArguments("--disable-notifications");
-
-            options.addArguments("--disable-extensions");
-
+            // Disable Password Manager Popup
             Map<String, Object> prefs = new HashMap<>();
             prefs.put("credentials_enable_service", false);
             prefs.put("profile.password_manager_enabled", false);
-            prefs.put("profile.password_manager_leak_detection", false);
-
             options.setExperimentalOption("prefs", prefs);
+
+            // Disable Automation Banner
+            options.setExperimentalOption(
+                    "excludeSwitches",
+                    new String[]{"enable-automation"}
+            );
+
+            options.setExperimentalOption(
+                    "useAutomationExtension",
+                    false
+            );
+
+            // Jika dijalankan di GitHub Actions
+            if (System.getenv("CI") != null) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+            }
 
             driver = new ChromeDriver(options);
 
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage()
+                    .timeouts()
+                    .implicitlyWait(Duration.ofSeconds(10));
+
         }
 
         return driver;
@@ -50,4 +68,5 @@ public class DriverFactory {
         }
 
     }
+
 }
